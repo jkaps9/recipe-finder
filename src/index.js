4574e36e7d4e8f26c690f1b0639b49e1caf5922e
Josh K Laptop <15219491+jkaps9@users.noreply.github.com>
2025-08-21 18:06:51 -0400

    add interactivity to dropdown button

--- src/index.js
+++ src/index.js
@@ -6,3 +6,9 @@ const hamburger = document.querySelector(".header__hamburger");
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
+
+const maxPrepDropdown = document.querySelector("legend#max-prep-time");
+
+maxPrepDropdown.addEventListener('click', () => {
+  maxPrepDropdown.classList.toggle("active");
+})
